634e9092e3107ac42684da5ac8dbcb2fd4d17c1e
lpraz <lpraz@mts.net>
2017-05-05 22:42:30 -0500

    Made progress on parse_args

---
+++ include/ArgumentType.hpp
@@ -0,0 +1,9 @@
+/* ArgumentType.hpp
+ * Defines the ArgumentType enum used to parse command-line arguments
+ * specifying tags or files.
+ */
+
+enum ArgumentType {
+    TAG_ARG,
+    FILE_ARG
+};
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 /* Stdlib includes */
 #include <cstring>
 #include <iostream>
+#include <vector>
+
+/* Local includes */
+#include "ArgumentType.hpp"
 
 /* Namespaces */
 using namespace std;
@@ -20,26 +24,39 @@ int main(int argc, char* argv[]) {
 
 /* Performs actions depending on the arguments passed to the program. */
 void parse_args(int argc, char* argv[]) {
-    if (argc == 1) { /* No args except for program name, give help */
+    if (argc == 1 || strcmp(argv[1], "help") == 0) {
+        // Show help
         cout << "tiger is a program for tagging and organizing files."
                 << endl << endl
                 << "Commands:" << endl
+                << "    help: Display this screen." << endl
                 << "    tag: Add a tag to a file." << endl
                 << "    search: Look up files with a certain tag." << endl;
-    } else if (strcmp(argv[1], "tag") == 0) { /* Tag a file */
+    } else if (strcmp(argv[1], "tag") == 0) {
+        // Add tag(s) to file(s)
+        
+        // Specification:
         // args after "-t": tags to add
         // args after "-f": files to add tags to
-        int collectingType;
+        
+        std::vector<std::string> tags;
+        std::vector<std::string> files;
+        ArgumentType type;
         
         for (int i = 2; i < argc; i++) {
             if (strcmp(argv[i], "-t") == 0)
-                // TODO: set CollectingType using an enum
-                cout << "Switching to tags" << endl;
+                type = TAG_ARG;
             else if (strcmp(argv[i], "-f") == 0)
-                // TODO: set CollectingType using an enum
-                cout << "Switching to files" << endl;
+                type = FILE_ARG;
+            else if (type == TAG_ARG)
+                tags.push_back(argv[i]);
+            else if (type == FILE_ARG)
+                files.push_back(argv[i]);
         }
-    } else if (strcmp(argv[1], "search") == 0) { /* Search by tag */
-        // argv[2]: tag to search for
+    } else if (strcmp(argv[1], "search") == 0) {
+        // Search for files with a certain tag(s)
+        
+        // Specification:
+        // args after argv[1]: tags to search for
     }
 }
