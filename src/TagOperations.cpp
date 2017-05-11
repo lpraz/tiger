7122aabb699058aba8ecfb2d9e086ea3bb25e752
lpraz <lpraz@mts.net>
2017-05-10 21:55:57 -0500

    Moved some code from main to TagOperations

--- src/TagOperations.cpp
+++ src/TagOperations.cpp
@@ -8,20 +8,38 @@
 #include <vector>
 
 /* Local includes */
+#include "ArgumentType.hpp"
 #include "Tag.hpp"
 
 /* Include self */
 #include "TagOperations.hpp"
 
 /* public: */
+/* Responsible for adding tags to files based on command-line arguments. */
 bool TagOperations::tag(std::vector<std::string> args) {
-    /* TODO: implement this */
-    return false;
+    std::vector<std::string> tags;
+    std::vector<std::string> files;
+    ArgumentType type;
+    
+    // Make lists of tags to be applied, and files to apply them to
+    for (std::vector<std::string>::iterator iter = args.begin();
+            iter != args.end(); iter++) {
+        if (*iter == "-t")
+            type = TAG_ARG;
+        else if (*iter == "-f")
+            type = FILE_ARG;
+        else if (type == TAG_ARG)
+            tags.push_back(*iter);
+        else if (type == FILE_ARG)
+            files.push_back(*iter);
+    }
+    
+    return false; // TODO: not implemented
 }
 
+/* Reponsible for looking up files based on command-line arguments. */
 std::vector<std::string> TagOperations::search(
         std::vector<std::string> args) {
-    /* TODO: implement this */
     std::vector<std::string> vec;
-    return vec;
+    return vec; // TODO: not implemented
 }
--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,8 @@
 #include <vector>
 
 /* Local includes */
-#include "ArgumentType.hpp"
+#include "Tag.hpp"
+#include "TagOperations.hpp"
 
 /* Namespaces */
 using namespace std;
@@ -18,7 +19,13 @@ void parse_args(int argc, char* argv[]);
 
 /* Main function. */
 int main(int argc, char* argv[]) {
+    std::vector<Tag> tags;
+    
+    // TODO: fill tags from file
+    
+    // Do the expected action based on arguments
     parse_args(argc, argv);
+    
     return 0;
 }
 
@@ -33,30 +40,10 @@ void parse_args(int argc, char* argv[]) {
                 << "    tag: Add a tag to a file." << endl
                 << "    search: Look up files with a certain tag." << endl;
     } else if (strcmp(argv[1], "tag") == 0) {
-        // Add tag(s) to file(s)
-        
-        // Specification:
-        // args after "-t": tags to add
-        // args after "-f": files to add tags to
-        
-        std::vector<std::string> tags;
-        std::vector<std::string> files;
-        ArgumentType type;
-        
-        for (int i = 2; i < argc; i++) {
-            if (strcmp(argv[i], "-t") == 0)
-                type = TAG_ARG;
-            else if (strcmp(argv[i], "-f") == 0)
-                type = FILE_ARG;
-            else if (type == TAG_ARG)
-                tags.push_back(argv[i]);
-            else if (type == FILE_ARG)
-                files.push_back(argv[i]);
-        }
+        std::vector<std::string> args(argv + 2, argv + argc);
+        TagOperations::tag(args);
     } else if (strcmp(argv[1], "search") == 0) {
-        // Search for files with a certain tag(s)
-        
-        // Specification:
-        // args after argv[1]: tags to search for
+        std::vector<std::string> args(argv + 2, argv + argc);
+        TagOperations::search(args);
     }
 }
