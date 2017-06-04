73791b5cc232b4096377aaeb6c00ef73f650ebe1
lpraz <lpraz@mts.net>
2017-06-03 19:25:50 -0500

    Added add/remove functions to TagFile

--- src/TagFile.cpp
+++ src/TagFile.cpp
@@ -6,6 +6,9 @@
  * Defines methods for the TagFile class.
  */
 
+// Stdlib includes
+#include <algorithm>
+
 // Include own header
 #include "TagFile.hpp"
 
@@ -31,5 +34,46 @@ namespace Tiger {
     std::unordered_map<std::string, std::vector<std::string>>
             TagFile::getTags(void) {
         return tags;
+    }
+    
+    /**
+     * Adds a tag to a file.
+     * In terms of the internal operations of the class, it more so
+     * adds a file to a tag, but the end-user should see no difference.
+     * If the tag doesn't exist, it will be added first, then the file
+     * added to its list.
+     *
+     * @param tag The tag to be added.
+     * @param file The file to add the tag to.
+     */
+    void TagFile::addTagToFile(std::string tag, std::string file) {
+        if (tags.find(tag) == tags.end())
+            tags[tag] = std::vector<std::string> {file};
+        else
+            tags[tag].push_back(file);
+    }
+    
+    /**
+     * Removes a tag from a file.
+     * In terms of the internal operations of the class, it more so
+     * removes a file from a tag, but the end-user should see no
+     * difference. If the tag would have no more files after having
+     * the file removed, it will be deleted.
+     *
+     * @param tag The tag to be removed.
+     * @param file The file to remove the tag from.
+     * @returns True if the file has the tag, false otherwise.
+     */
+    bool TagFile::removeTagFromFile(std::string tag, std::string file) {
+        if (tags.find(tag) == tags.end()) {
+            auto index = std::remove(tags[tag].begin(), tags[tag].end(),
+                    file);
+            if (index != tags[tag].end()) {
+                tags[tag].erase(index);
+                return true;
+            }
+        }
+        
+        return false;
     }
 }
